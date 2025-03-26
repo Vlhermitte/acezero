1ed2654cb1cea3dd5a8d8fbd8bc66b620e782475
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-03-26 09:57:11 +0100

    Refactor image processing in convert_to_colmap.py to use scipy for rotation and update default directory paths

--- convert_to_colmap.py
+++ convert_to_colmap.py
@@ -4,6 +4,7 @@ import numpy as np
 import trimesh
 import argparse
 import pycolmap
+from scipy.spatial.transform import Rotation
 from PIL import Image as PILImage
 from typing import Tuple, List
 
@@ -19,13 +20,17 @@ def read_pose_file(pose_file) -> Tuple[List[pycolmap.Image], List[pycolmap.Camer
             img_path = '../' + '/'.join(img_path)
             with PILImage.open(img_path) as pil_img:
                 width, height = pil_img.size
-            img_basename_without_ext = os.path.basename(img_path).split('.')[0]
+            img_basename = os.path.basename(img_path)
             qw, qx, qy, qz = [float(t) for t in tokens[1:5]]
             tx, ty, tz = [float(t) for t in tokens[5:8]]
             focal_length = float(tokens[8])
             confidence = float(tokens[9])
 
-            rotation = pycolmap.Rotation3d([qx, qy, qz, qw])
+            R = Rotation.from_quat([qx, qy, qz, qw]).as_matrix()
+            t = np.array([tx, ty, tz])
+            T = np.eye(3, 4)
+            T[:, :3] = R
+            T[:, 3] = t
 
             camera = pycolmap.Camera(
                 camera_id=i,
@@ -37,8 +42,8 @@ def read_pose_file(pose_file) -> Tuple[List[pycolmap.Image], List[pycolmap.Camer
             image = pycolmap.Image(
                 image_id=i,
                 camera_id=i,
-                name=img_basename_without_ext,
-                cam_from_world=pycolmap.Rigid3d(rotation, [tx, ty, tz]),
+                name=img_basename,
+                cam_from_world=pycolmap.Rigid3d(T),
                 registered=True if confidence >= 1000 else False
             )
             cameras.append(camera)
@@ -51,12 +56,12 @@ if __name__ == '__main__':
     parser.add_argument(
         '--src_dir',
         type=str,
-        default='../data/results/acezero/ETH3D/courtyard/acezero_format/',
+        default='../data/results/acezero/TanksAndTemples/Ignatius/acezero_format/',
         help='source directory'
     )
     parser.add_argument(
         '--dst_dir',
-        default='../data/results/acezero/ETH3D/courtyard/colmap/sparse/0',
+        default='../data/results/acezero/TanksAndTemples/Ignatius/colmap/sparse/0',
         type=str,
         help='destination directory'
     )
